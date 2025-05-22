962c6ab8334bf8813a744475d1f751ea8d8799a7
ThezGianTz <samuel16.ti@hotmail.com>
2025-05-22 17:06:54 -0300

    aprendendo prompt

---
+++ logicacomjs.js
@@ -0,0 +1,13 @@
+/*aaaaaaaaaa
+asasasas
+asknjoasjhasjhashilsd
+jasihashipdspwhiqsq
+sjhhiqasugaugaiapsda
+ihwuidwusdçilcsx
+hzxajaixajxaijxa
+anajsqapxaçsda*/
+
+let salario = prompt("Digite seu salário:")
+let mestrab = prompt("Digite quantos meses você trabalhou:")
+let decimo = salario/12*mestrab
+alert("Seu décimo é: R$" + decimo )
