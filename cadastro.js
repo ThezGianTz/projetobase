ba2b21fd3d7325a9ae5e6be37ce34c39190644a0
ThezGianTz <samuel16.ti@hotmail.com>
2025-06-12 11:31:19 -0300

    document e events

---
+++ cadastro.js
@@ -0,0 +1,12 @@
+function clicou(){
+    let quandoclicar = document.getElementById("botao")
+    quandoclicar.innerText = "E ai vai enviar não?"
+}
+function saiu(){
+    let quandosair = document.getElementById("botao")
+    quandosair.style.background = "green"
+}
+function entrar(){
+    let quandoentrar = document.getElementById("botao")
+    quandoentrar.style.background = "red"
+}
